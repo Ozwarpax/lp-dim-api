7ee56d5b54b9c80d7ac1ae1d4563a00bd5fa75af
jérémy_laurent <jeremy.laurentj@gmail.com>
2022-03-07 18:18:35 +0100

    school

---
+++ app.js
@@ -0,0 +1,36 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var studentRouter = require("./routes/student");
+
+
+var app = express();
+var mongoose = require("mongoose");
+var connexionStringLocal = "mongodb+srv://jeremy:Pq0t6sUSYYbt0DMY@iut.kug8z.mongodb.net/School?retryWrites=true&w=majority";
+
+var mongodb = process.env.MONGO_URI || connexionStringLocal;
+mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+app.use("/student", studentRouter);
+
+module.exports = app;

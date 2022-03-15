c6fbb6486ef9b519c354056934882a012cfcc6e3
jérémy_laurent <jeremy.laurentj@gmail.com>
2022-03-15 15:46:36 +0100

    zae

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors')
 var studentRouter = require("./routes/student");
 
 
@@ -12,7 +12,7 @@ var mongoose = require("mongoose");
 var connexionStringLocal = "mongodb+srv://jeremy:Pq0t6sUSYYbt0DMY@iut.kug8z.mongodb.net/School?retryWrites=true&w=majority";
 
 var mongodb = process.env.MONGO_URI || connexionStringLocal;
-mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.promise = global.Promise;
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -21,16 +21,15 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
+app.use(cors())
 
 app.use("/student", studentRouter);
+
+
 
 module.exports = app;
